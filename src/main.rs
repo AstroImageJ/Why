e09c3335eadc2d4eeeefab9d67688a9431197453
Kevin Eastridge <kevbeastridge@gmail.com>
2022-03-22 09:16:31 -0400

    Make test more sensible

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,12 @@ use crate::launch_config::LauncherConfig;
 fn main() {
     println!("Starting application!");
     // todo comment when publishing
+    //std::env::set_current_dir("./test");
+
     launch();
-    //test2();
 }
 
 fn launch() {
-    let mut m = LaunchOpts {
-        config: LauncherConfig {
-            ..LauncherConfig::read_file()
-        },
-        jvm_opts: vec![],//this can be relative
-        program_opts: env::args().collect() // Forward launch args to the app
-    };
-
-    m.jvm_opts.append(&mut m.config.read_launch_opts());
-    if m.config.classpath.is_some() {
-        m.jvm_opts.push("-Djava.class.path=".to_string() + &*m.config.classpath.as_ref().unwrap());
-    }
-
-    create_and_run_jvm(&m)
-}
-
-fn test2() {
-    std::env::set_current_dir("./test");
-
     let mut m = LaunchOpts {
         config: LauncherConfig {
             ..LauncherConfig::read_file()
