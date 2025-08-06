4da228ce101297419fc2cd49cb208dba66030ee1
keastrid <70925169+keastrid@users.noreply.github.com>
2025-08-06 19:54:33 -0400

    Cleanup launch sequence

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use crate::display_handler::message;
 use crate::file_handler::get_app_dir_path;
 use crate::java_launcher::{create_and_run_jvm, LaunchOpts};
 use crate::launch_config::{parse_config, process_config};
-use crate::manifest_handler::read_manifest;
-use std::path::PathBuf;
 use std::env;
 
 mod display_handler;
@@ -12,34 +10,21 @@ mod java_launcher;
 mod launch_config;
 mod manifest_handler;
 
-pub const DEBUG: bool = true;
+pub const DEBUG: bool = false;
 
 /// Entrypoint
 fn main() {
-    println!("Launcher starting!");
-
-    // todo true when publishing
-    if true {
-        correct_directory();
-    } else {
-        env::set_current_dir("./test").expect("could not set test directory");
-        println!("{:?}", env::current_dir());
+    if DEBUG {
+        println!("Launcher starting!");
     }
+
+    correct_directory();
 
     launch();
 }
 
 /// Setup the environment and launch the application
 fn launch() {
-    let cfgPath = env::current_exe().unwrap().with_extension("cfg");
-    println!("{:?}", cfgPath);
-
-    let mf = read_manifest(PathBuf::from("ij.jar"));
-    println!("{:?}", mf);
-
-    let mf = read_manifest(PathBuf::from("ij"));
-    println!("{:?}", mf);
-
     let cfgPath = get_app_dir_path().join(
         env::current_exe()
             .unwrap()
@@ -47,41 +32,43 @@ fn launch() {
             .file_name()
             .unwrap(),
     );
-    println!("{:?}", cfgPath);
+
+    // Set current directory to app folder
     let _ = env::set_current_dir(get_app_dir_path());
 
+    if !cfgPath.exists() {
+        message(format!("Failed to find configuration file at {:?}", cfgPath).as_str())
+    }
+
     // Build launch opts
-    let mut m = LaunchOpts {
-        config: process_config(&parse_config(cfgPath).unwrap()), //todo handle missing file error
-        jvm_opts: vec![],                                        //this can be relative
-        program_opts: env::args().collect(),                     // Forward launch args to the app
+    let mut launch_options = LaunchOpts {
+        config: process_config(&parse_config(cfgPath).unwrap()),
+        jvm_opts: vec![],
+        program_opts: env::args().collect(), // Forward launch args to the app
     };
 
     // The first element is the launcher path, no need to pass it on
-    if m.program_opts.len() >= 1 {
-        m.program_opts.remove(0);
+    if launch_options.program_opts.len() >= 1 {
+        launch_options.program_opts.remove(0);
     }
 
-    // Build classpath
-    m.jvm_opts.append(&mut m.config.java_opts);
-    println!("{:?}", m);
-    println!("{:?}", m.jvm_opts);
-    /*if m.config.classpath.is_some() {
-        m.jvm_opts
-            .push("-Djava.class.path=".to_string() + &*m.config.classpath.as_ref().unwrap());
-    }*/
+    // Forward jvm options to primary config struct
+    launch_options.jvm_opts.append(&mut launch_options.config.java_opts);
+
+    if DEBUG {
+        println!("{:?}", launch_options);
+    }
 
     // Run the app
-    create_and_run_jvm(&m)
+    create_and_run_jvm(&launch_options)
 }
 
 /// This makes sure the current working directory is the exe's home.<br>
 /// This can differ from the current working directory in cases where you are running the exe
-/// from command line or script from a different location.
-//todo set to app/ directory made by jpackage
+/// from the command line or script from a different location.
 fn correct_directory() {
     // This gets the location of the exe file, not its current working directory
-    // These can differ if say running the exe through command line when in a different folder
+    // These can differ if say running the exe through the command line when in a different folder
     let exe_home = env::current_exe();
     if let Ok(exe_home) = exe_home {
         if let Some(exe_home) = exe_home.parent() {
