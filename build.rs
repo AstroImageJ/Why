b60b35e8e1ea97d9cf3963e6d5d21b6f21a6a5e6
keastrid <70925169+keastrid@users.noreply.github.com>
2026-07-07 18:15:30 -0400

    Add aij icon

--- build.rs
+++ build.rs
@@ -2,12 +2,18 @@ use embed_manifest::{embed_manifest, new_manifest};
 use embed_manifest::manifest::DpiAwareness;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=aij.ico");
+
     // Set DPI awareness as per monitor
     // Done in manifest as the documentation says to prefer it over doing so in code
     // https://learn.microsoft.com/en-us/windows/win32/hidpi/setting-the-default-dpi-awareness-for-a-process
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
         embed_manifest(new_manifest("Why.JavaLauncher").dpi_awareness(DpiAwareness::PerMonitorV2))
             .expect("unable to embed manifest file");
+
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("aij.ico");
+        res.compile().expect("failed to compile resources");
     }
-    println!("cargo:rerun-if-changed=build.rs");
 }
